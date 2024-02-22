6140436d4463139d0acd74abd89f0f163a4ed468
qzkuan <qzkuan@gmail.com>
2024-02-22 16:06:02 +0800

    modify0001

--- src/topinterviewquestions/Problem_0001_TwoSum.java
+++ src/topinterviewquestions/Problem_0001_TwoSum.java
@@ -3,7 +3,15 @@ package topinterviewquestions;
 import java.util.HashMap;
 
 public class Problem_0001_TwoSum {
-
+	/**
+	 * [1. 两数之和](https://leetcode.cn/problems/two-sum/)
+	 * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
+	 * 假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+	 * 
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
 	public static int[] twoSum(int[] nums, int target) {
 		// key 某个之前的数   value 这个数出现的位置
 		HashMap<Integer, Integer> map = new HashMap<>();
